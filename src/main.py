94dbf61e83ed9c2c317a1adfffac70fbea623935
Robert Amelard <rob.amelard@gmail.com>
2020-09-23 12:20:54 -0400

    Working network with parallel demographics input layer.

--- src/main.py
+++ src/main.py
@@ -1,61 +1,86 @@
-# TODO: install GPU requirements for TF 2.3.0
-
-from tensorflow.keras.callbacks import Callback
+import tensorflow as tf
+from tensorflow.keras.layers import Dense, Activation, Input, Embedding
+from tensorflow.keras.callbacks import Callback, TensorBoard
+from tensorflow.keras import Model, Input, optimizers
 import numpy as np
+from datetime import datetime
 import util
+import pandas as pd
 
-from tcn import compiled_tcn
+from tcn import TCN
 
+# # TODO: disabling GPU
+# tf.config.set_visible_devices([], 'GPU')
 
-def data_generator(n, seq_length):
-    """
-    Args:
-        seq_length: Length of the adding problem data
-        n: # of data in the set
-    """
-    x_num = np.random.uniform(0, 1, (n, 1, seq_length))
-    x_mask = np.zeros([n, 1, seq_length])
-    y = np.zeros([n, 1])
-    for i in range(n):
-        positions = np.random.choice(seq_length, size=2, replace=False)
-        x_mask[i, 0, positions[0]] = 1
-        x_mask[i, 0, positions[1]] = 1
-        y[i, 0] = x_num[i, 0, positions[0]] + x_num[i, 0, positions[1]]
-    x = np.concatenate((x_num, x_mask), axis=1)
-    x = np.transpose(x, (0, 2, 1))
-    return x, y
+seq_len = 20
+feature_list = ['WR', 'HR', 'VE', 'BF', 'HRR']
+seq_step_train = 5
+vo2_type = 'VO2'
+x_train, x_train_static, y_train, x_val, x_val_static, y_val, x_test, x_test_static, y_test = \
+    util.get_x_y(seq_len, feature_list, seq_step_train, vo2_type)
+print('x_train: {}\nx_val: {}\nx_test: {}'.format(x_train.shape, x_val.shape, x_test.shape))
+print('seq_len: {}\nfeature_list: {}'.format(seq_len, feature_list))
 
-
-def get_x_y(size=1000):
-    x_train, y_train = data_generator(n=200000, seq_length=600)
-    x_test, y_test = data_generator(n=40000, seq_length=600)
-    return x_train, y_train, x_test, y_test
-
-
-# x_train, y_train, x_test, y_test = get_x_y()
-x_train, y_train, x_test, y_test = util.get_x_y()
+# TCN parameters
+nb_filters = 24
+kernel_size = 8
+dilations = [2 ** i for i in range(4)]
+dropout_rate = 0.2
+batch_size = 32
+epochs = 25
+note = 'with demographics (first time)'  # note describing the setup
 
 
 class PrintSomeValues(Callback):
 
     def on_epoch_begin(self, epoch, logs={}):
         print('y_true, y_pred')
-        print(np.hstack([y_test[:5], self.model.predict(x_test[:5])]))
+        print(np.hstack([y_test[:5], self.model.predict([x_test[:5], x_test_static[:5]])]))
 
 
 def run_task():
-    model = compiled_tcn(return_sequences=False,
-                         num_feat=x_train.shape[2],
-                         num_classes=0,
-                         nb_filters=24,
-                         kernel_size=8,
-                         dilations=[2 ** i for i in range(9)],
-                         nb_stacks=1,
-                         max_len=x_train.shape[1],
-                         use_skip_connections=False,
-                         regression=True,
-                         dropout_rate=0.2,
-                         use_layer_norm=True)
+    # "The most important factor for picking parameters is to make sure that
+    # the TCN has a sufficiently large receptive field by choosing k and d
+    # that can cover the amount of context needed for the task." (Bai 2018)
+    # Receptive field = nb_stacks * kernel_size * last_dilation
+    num_feat = x_train.shape[2]
+    max_len = x_train.shape[1]
+    lr = 0.002
+    input_layer = Input(shape=(max_len, num_feat))
+    x = TCN(nb_filters, kernel_size, 1, dilations, 'causal',
+            False, dropout_rate, False,
+            'relu', 'he_normal', False, True,
+            name='tcn')(input_layer)
+
+    # Add static demographic info
+    nb_static = x_train_static.shape[1]
+    # TODO: use Embedding layer with one-hot indexing, or use directly.
+    input_layer_static = Input(shape=(nb_static,))
+    #y = Embedding(nb_static, nb_static)(input_layer_static)
+
+    xy = tf.keras.layers.concatenate([x, input_layer_static])
+
+    z = Dense(1)(xy)
+    z = Activation('linear')(z)
+    output_layer = z
+    model = Model([input_layer, input_layer_static], output_layer)
+    opt = optimizers.Adam(lr=lr, clipnorm=1.)
+    model.compile(opt, loss='mean_squared_error')
+    print('model.x = {}, {}'.format(input_layer.shape, input_layer_static.shape))
+    print('model.y = {}'.format(output_layer.shape))
+
+    # model = compiled_tcn(return_sequences=False,
+    #                      num_feat=x_train.shape[2],
+    #                      num_classes=0,
+    #                      nb_filters=nb_filters,
+    #                      kernel_size=kernel_size,
+    #                      dilations=dilations,
+    #                      nb_stacks=1,
+    #                      max_len=x_train.shape[1],
+    #                      use_skip_connections=False,
+    #                      regression=True,
+    #                      dropout_rate=dropout_rate,
+    #                      use_layer_norm=True)
 
     print(f'x_train.shape = {x_train.shape}')
     print(f'y_train.shape = {y_train.shape}')
@@ -66,8 +91,37 @@ def run_task():
     # http://chappers.github.io/web%20micro%20log/2017/01/26/quick-models-in-keras/
     model.summary()
 
-    model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=15,
-              batch_size=256, callbacks=[psv])
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M%S')
+    logdir = 'logs/log' + timestamp
+    tensorboard = TensorBoard(log_dir=logdir, update_freq='epoch', profile_batch=0)
+
+    history = model.fit([x_train, x_train_static], y_train, validation_data=([x_val, x_val_static], y_val),
+                        epochs=epochs, batch_size=batch_size, callbacks=[psv, tensorboard])
+    mdl_dir = 'models/mdl' + timestamp
+    model.save(mdl_dir)
+
+    # Save network options for repeatability/understandability.
+    opts = {'seq_len': seq_len, 'feature_list': feature_list, 'seq_step_train': seq_step_train, 'vo2_type': vo2_type,
+            'nb_filters': nb_filters, 'kernel_size': kernel_size, 'dilations': dilations, 'dropout_rate': dropout_rate,
+            'batch_size': batch_size, 'epochs': epochs, 'note': note}
+    csv_file = mdl_dir + '/network_params.csv'
+    pd.DataFrame.from_dict(data=opts, orient='index').to_csv(csv_file, header=False)
+
+    #ckpt = tf.train.Checkpoint(step=tf.Variable(1), optimizer=opt, net=net, iterator=iterator)
+    #manager = tf.train.CheckpointManager(ckpt, './ckpts', max_to_keep=3)
+
+    #model.evaluate(x_test, y_test, verbose=2)
+
+    pred = model.predict([x_val, x_val_static])
+    import plotly.graph_objects as go
+    from plotly.subplots import make_subplots
+    #fig = make_subplots(specs=[[{"secondary_y": True}]])
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(y=y_val.flatten(), line_color='rgb(0.2,0.2,0.2)', name='y'))
+    fig.add_trace(go.Scatter(y=pred.flatten(), line_color='rgba(255,0,0,0.8)', name='y_hat'))
+    fig.update_layout(title=mdl_dir)
+    fig.show()
+    fig.write_html(mdl_dir + '/plotly.html')
 
 
 if __name__ == '__main__':
--- src/util.py
+++ src/util.py
@@ -1,10 +1,9 @@
 import pandas as pd
-import tensorflow as tf
 import numpy as np
 import os
 import re
 import math
-# plotly?
+import csv
 
 
 def generate_rolling_sequence(features, vo2, seq_len, step=5):
@@ -23,39 +22,85 @@ def generate_rolling_sequence(features, vo2, seq_len, step=5):
     return x, y
 
 
-def get_x_y_helper(seq_len, feature_list, data_type='train'):
+def _get_x_y_helper(seq_len, feature_list, seq_step=5, data_type='train', vo2_type='VO2'):
     data_dir = '../data/{}/'.format(data_type)
     csv_files = os.listdir(data_dir)
-    # Determine unique participant ids from one of the protocols (high)
-    high_matches = [re.search('high(\d+).csv', f) for f in csv_files if f.startswith('high')]
+    # Determine full set of unique participant ids
+    high_matches = [re.search('(\d+).csv', f) for f in csv_files if f.startswith('high')]
     pids = sorted([int(m.group(1)) for m in high_matches])
+    pids = list(set(pids))  # unique ids
 
-    def load_data(pid, protocol):
+    # TODO: use either filenames or pids and get rid of the other.
+
+    def load_data_pid(pid, protocol):
         # e.g.: high1.csv
-        filename = data_dir + '{}{}.csv'.format(protocol, pid)
-        df = pd.read_csv(filename)
+        filename = '{}{}.csv'.format(protocol, pid)
+        try:
+            df = pd.read_csv(data_dir + filename)
+        except FileNotFoundError:
+            return None, None
+
         # Columns: WR,HR,DeltaHR,VE,BF,VO2rel,HRR,VO2
-        vo2 = df['VO2'].to_numpy()
-        vo2_rel = df['VO2rel'].to_numpy
+        vo2 = df[vo2_type].to_numpy()
         features = df[feature_list].to_numpy()
-        x, y = generate_rolling_sequence(features, vo2, seq_len, step=5)
-        return x, y
+        x_, y_ = generate_rolling_sequence(features, vo2, seq_len, step=seq_step)
+        return x_, y_
+
+    def load_data(filename):
+        # e.g.: high1.csv
+        # filename = data_dir + '{}{}.csv'.format(protocol, pid)
+        try:
+            df = pd.read_csv(data_dir + filename)
+        except FileNotFoundError:
+            return None, None
+
+        # Columns: WR,HR,DeltaHR,VE,BF,VO2rel,HRR,VO2
+        vo2 = df[vo2_type].to_numpy()
+        features = df[feature_list].to_numpy()
+        x_, y_ = generate_rolling_sequence(features, vo2, seq_len, step=seq_step)
+        return x_, y_
 
     x = np.array([]).reshape(0, seq_len, len(feature_list))
     y = np.array([]).reshape(0, 1)
-    for pid in pids:
-        xhi, yhi = load_data(pid, 'high')
-        xmid, ymid = load_data(pid, 'mid')
-        xlo, ylo = load_data(pid, 'low')
+    xstatic = np.array([]).reshape(0, 3)
+    # for pid in pids[:2]:
+    df = pd.read_csv(data_dir + 'demographics.csv')
+    demogs = {p: [h, w, a] for p, h, w, a in zip(df['Participant'], df['Height'], df['Weight'], df['Age'])}
+    for f in csv_files:
+        if f == 'demographics.csv':
+            continue
+        xi, yi = load_data(f)
 
-        x = np.concatenate((x, xhi), axis=0)
-        y = np.concatenate((y, yhi), axis=0)
-        x = np.concatenate((x, xhi, xmid, xlo), axis=0)
-        y = np.concatenate((y, yhi, ymid, ylo))
-    return x, y
+        match = re.search('(\d+).csv', f)
+        pid = int(match.group(1))
+        xstatic_i = np.array(demogs[pid]).reshape(1, 3)
+        # Repeat these demographics for all of this participant's data
+        xstatic_i = np.tile(xstatic_i, (xi.shape[0], 1))
+
+        #print('{}: {}-{}'.format(f, x.shape[0], x.shape[0]+xi.shape[0]))
+        x = np.concatenate((x, xi), axis=0)
+        y = np.concatenate((y, yi))
+
+        xstatic = np.concatenate((xstatic, xstatic_i), axis=0)
+
+        # TODO: uncomment low and mid
+        # xhi, yhi = load_data(pid, 'high')
+        # xmid, ymid = load_data(pid, 'mid')
+        # xlo, ylo = load_data(pid, 'low')
+        # x = np.concatenate((x, xhi, xmid, xlo), axis=0)
+        # y = np.concatenate((y, yhi, ymid, ylo))
+    return x, xstatic, y
 
 
-def get_x_y(seq_len=10, feature_list=['HR', 'VE', 'BF', 'HRR']):
+def get_x_y(seq_len=10, feature_list=['HR', 'VE', 'BF', 'HRR'], seq_step_train=5, vo2_type='VO2'):
+    """
+    Generates train and test sequences.
+    :param seq_len: sequence length (s)
+    :param feature_list: headers from .csv files
+    :param seq_step_train: step between sequences during training
+    :param vo2_type: VO2 or VO2_rel
+    :return: xtrain, ytrain, xval, yval, xtest, ytest where x [n,seq_len,nb_feat], y [n,1]
+    """
     # https://towardsdatascience.com/time-series-forecasting-with-recurrent-neural-networks-74674e289816
     # read all vo2, generate xtrain,ytrain,xtest,ytest
     # note: don't use overlapping participants
@@ -64,11 +109,29 @@ def get_x_y(seq_len=10, feature_list=['HR', 'VE', 'BF', 'HRR']):
     # I think (x,y) should be (N,L,F) and (N,1), N=#sequences, L=sequence length, F=#feature channels
 
     # https://www.tensorflow.org/guide/data
+    # https://www.tensorflow.org/tutorials/structured_data/time_series
+    # https://www.tensorflow.org/tutorials/load_data/csv
     # If all of your input data fits in memory, the simplest way to create a Dataset from them is to convert them to tf.Tensor objects and use Dataset.from_tensor_slices().
 
-    xtrain, ytrain = get_x_y_helper(seq_len, feature_list, data_type='train')
-    xtest, ytest = get_x_y_helper(seq_len, feature_list, data_type='test')
-    return xtrain, ytrain, xtest, ytest
+    xtrain, xtrain_static, ytrain = _get_x_y_helper(seq_len, feature_list, seq_step=seq_step_train, data_type='train', vo2_type=vo2_type)
+    # Get every sequence in the data for testing
+    xval, xval_static, yval = _get_x_y_helper(seq_len, feature_list, seq_step=1, data_type='validation', vo2_type=vo2_type)
+    xtest, xtest_static, ytest = _get_x_y_helper(seq_len, feature_list, seq_step=1, data_type='test', vo2_type=vo2_type)
+
+    # TODO: change to estimators? https://towardsdatascience.com/how-to-normalize-features-in-tensorflow-5b7b0e3a4177
+    mu = np.mean(xtrain, axis=0)
+    sigma = np.std(xtrain, axis=0)
+    xtrain = (xtrain-mu)/sigma
+    xval = (xval-mu)/sigma
+    xtest = (xtest-mu)/sigma
+
+
+    return xtrain, xtrain_static, ytrain, xval, xval_static, yval, xtest, xtest_static, ytest
+
+
+def load_demographics():
+
+    return dtrain, dval
 
 
     # dataset = tf.data.Dataset.from_tensor_slices((df.to_numpy(), vo2.to_numpy()))
