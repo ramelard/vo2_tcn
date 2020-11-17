fbc6f0166be9991c74a38767fe9a5fb397f8e37f
Robert Amelard <rob.amelard@gmail.com>
2020-11-17 10:47:27 -0500

    Parameter to choose data normalization strategy.

--- src/util.py
+++ src/util.py
@@ -125,13 +125,14 @@ def _get_x_y_helper(seq_len, feature_list, seq_step=5, data_type='train', vo2_ty
     return x, xstatic, y, descr
 
 
-def get_x_y(seq_len=10, feature_list=['WR', 'HR', 'VE', 'BF', 'HRR'], seq_step_train=5, vo2_type='VO2'):
+def get_x_y(seq_len=10, feature_list=['WR', 'HR', 'VE', 'BF', 'HRR'], seq_step_train=5, vo2_type='VO2', norm_type='mixed'):
     """
     Generates train and test sequences.
     :param seq_len: sequence length (s)
     :param feature_list: headers from .csv files
     :param seq_step_train: step between sequences during training
     :param vo2_type: VO2 or VO2rel
+    :param norm_type: mixed (WR norm, standardize other), norm, or stand
     :return: xtrain, ytrain, xval, yval, xtest, ytest where x [n,seq_len,nb_feat], y [n,1]
     """
     # https://towardsdatascience.com/time-series-forecasting-with-recurrent-neural-networks-74674e289816
@@ -139,7 +140,7 @@ def get_x_y(seq_len=10, feature_list=['WR', 'HR', 'VE', 'BF', 'HRR'], seq_step_t
     # note: don't use overlapping participants
     # vo2_data = df['vo2'].values
 
-    # I think (x,y) should be (N,L,F) and (N,1), N=#sequences, L=sequence length, F=#feature channels
+    # (x,y) is (N,L,F) and (N,1), N=#sequences, L=sequence length, F=#feature channels
 
     # https://www.tensorflow.org/guide/data
     # https://www.tensorflow.org/tutorials/structured_data/time_series
@@ -157,9 +158,27 @@ def get_x_y(seq_len=10, feature_list=['WR', 'HR', 'VE', 'BF', 'HRR'], seq_step_t
     # TODO: change to estimators? https://towardsdatascience.com/how-to-normalize-features-in-tensorflow-5b7b0e3a4177
     mu = np.mean(xtrain[:, 0, :], axis=0)
     sigma = np.std(xtrain[:, 0, :], axis=0)
-    xtrain = (xtrain-mu)/sigma
-    xval = (xval-mu)/sigma
-    xtest = (xtest-mu)/sigma
+    min_ = np.min(xtrain[:, 0, :], axis=0)
+    max_ = np.max(xtrain[:, 0, :], axis=0)
+    if norm_type == 'norm':
+        numer = min_
+        denom = max_ - min_
+    elif norm_type == 'stand':
+        numer = mu
+        denom = sigma
+    else:
+        numer = mu
+        denom = sigma
+        if 'WR' in feature_list:
+            idx = feature_list.index('WR')
+            numer[idx] = min_[idx]
+            denom[idx] = max_[idx] - min_[idx]
+    xtrain = (xtrain - numer) / denom
+    xval = (xval - numer) / denom
+    xtest = (xtest - numer) / denom
+    # xtrain = (xtrain-min_)/(max_-min_)
+    # xval = (xval-min_)/(max_-min_)
+    # xtest = (xtest-min_)/(max_-min_)
 
     train = {'x': xtrain, 'static': xtrain_static, 'y': ytrain, 'descr': train_descr}
     val = {'x': xval, 'static': xval_static, 'y': yval, 'descr': val_descr}
